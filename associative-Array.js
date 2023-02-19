db4ea3043282779da531d8a3107d8493f012d34f
Saba <sabanafchigar@gmail.com>
2023-02-19 12:55:30 +0330

    Add files via upload

---
+++ associative-Array.js
@@ -0,0 +1,6 @@
+let data=[]
+data["firstName"]="saba"
+data["lastName"]="Nafchigar"
+data["age"]=31
+console.log(data);
+console.log(`my name is ${data["firstName"]} and my last name is ${data["lastName"]} and my age is ${data["age"]}`);
